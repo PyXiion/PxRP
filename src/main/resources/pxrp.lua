cd82fa70785b28bd2897ee1d8997fbdf1502e7eb
PyXiion <me@pyxiion.ru>
2026-05-23 02:20:53 +0300

    fix: update pxrp.lua to use ctx API and fix syntax bugs
    - Migrate all handlers to accept ctx as first argument
    - Fix missing 'end' in coins() function (syntax error)
    - Fix payCoins using undeclared global 'player'
    - Switch messages to bilingual (English + Russian)

--- src/main/resources/pxrp.lua
+++ src/main/resources/pxrp.lua
@@ -2,50 +2,55 @@ require "format"
 require "simple"
 
 
-function fart()
+function fart(ctx)
+    local player = ctx.player
     local pos = player.pos
     local dir = player.bodyDir
     local now = mc.time()
     local last = player.data.lastFart or 0
 
     if now - last < 10 then
-        mc.broadcast("Подожди " .. (10 - (now - last)) .. " секунд!")
+        mc.broadcast("Wait " .. (10 - (now - last)) .. " seconds to fart again!")
         return
     end
 
     player.data.lastFart = now
 
-    broadcastFormat "*{p.name} пёрнул*" {p=player}
+    broadcastFormat "*{p.name} farted*" {p=player}
     mc.particle("minecraft:gust", pos.x - dir.x * 0.5, pos.y + 0.6, pos.z - dir.z * 0.5, player.world)
     mc.playSound("minecraft:entity.slime.squish", pos.x, pos.y, pos.z, player.world, 10, 0.1)
 end
 
-function kill(target)
-    broadcastFormat "*{p.name} убил(а) {t.name}*" {p=player, t=target}
+function kill(ctx, target)
+    broadcastFormat "*{p.name} killed {t.name}*" {p=ctx.player, t=target}
 end
 
-function coins()
-    local bal = player.data.coins or 0
-    mc.broadcast("У тебя " .. bal .. " монет")
+function coins(ctx)
+    local bal = ctx.player.data.coins or 0
+    if bal < 100 then
+        mc.broadcast("You have " .. bal .. " coins")
+    else
+        mc.broadcast("You have a lot of coins! (" .. bal .. ")")
+    end
 end
 
-function giveCoins()
-    player.data.coins = (player.data.coins or 0) + 10
-    mc.broadcast("+10 монет! Теперь у тебя " .. player.data.coins .. " монет")
+function giveCoins(ctx)
+    ctx.player.data.coins = (ctx.player.data.coins or 0) + 10
+    mc.broadcast("+10 coins!")
 end
 
-function payCoins(target)
+function payCoins(ctx, target)
     local amount = 10
-    local bal = player.data.coins or 0
+    local bal = ctx.player.data.coins or 0
 
     if bal < amount then
         mc.broadcast("Недостаточно монет! У тебя только " .. bal)
         return
     end
 
-    player.data.coins = bal - amount
+    ctx.player.data.coins = bal - amount
     target.data.coins = (target.data.coins or 0) + amount
-    mc.broadcast(player.name .. " передал " .. amount .. " монет " .. target.name)
+    mc.broadcast(ctx.player.name .. " передал " .. amount .. " монет " .. target.name)
 end
 
 function eventStats()
